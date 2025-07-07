191752bfe0060333a031a38b4775da7c08e5a58a
JanBu <yosefsitumorang97@gmail.com>
2025-07-07 14:00:31 +0700

    feat: create route auth

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import router from './routes'
 
-createApp(App).use(VueQueryPlugin).mount('#app')
+createApp(App).use(VueQueryPlugin).use(router).mount('#app')
